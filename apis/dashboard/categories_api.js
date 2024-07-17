bc9171f20e85eaa5a0ea3f518001989d5f82dc2b
LaNguAx <itayakni@gmail.com>
2024-07-17 16:00:13 +0300

    testing new branch

--- apis/dashboard/categories_api.js
+++ apis/dashboard/categories_api.js
@@ -1,5 +1,5 @@
-import CategoryService from '../../services/dashboard/category_service.js';
-import ProductsService from '../../services/dashboard/product_service.js';
+import CategoryService from "../../services/dashboard/category_service.js";
+import ProductsService from "../../services/dashboard/product_service.js";
 
 // Get all categories
 export async function getCategories(req, res) {
@@ -7,8 +7,8 @@ export async function getCategories(req, res) {
     const categories = await CategoryService.getCategories();
     res.status(200).json({ success: true, data: categories });
   } catch (error) {
-    console.error('Error fetching categories:', error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    console.error("Error fetching categories:", error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 }
 
@@ -20,8 +20,8 @@ export async function createCategory(req, res) {
     const newCategory = await CategoryService.createCategory(category);
     res.status(201).json({ success: true, data: newCategory });
   } catch (error) {
-    console.error('Error creating category:', error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    console.error("Error creating category:", error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 }
 
@@ -33,41 +33,41 @@ export async function getCategory(req, res) {
     if (!category) {
       return res
         .status(404)
-        .json({ success: false, message: 'Category not found' });
+        .json({ success: false, message: "Category not found" });
     }
     res.status(200).json({ success: true, data: category });
   } catch (error) {
-    console.error('Error fetching category:', error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    console.error("Error fetching category:", error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 }
 
 // Update a category by ID
 export async function updateCategory(req, res) {
   const { id } = req.params;
-  const newCategory = { ...req.body }
+  const newCategory = { ...req.body };
 
   // calculate the diffence between old vs new to delete any subcategories that have been deleted by user & delete the products associated with the delete subcategories
   const oldCategory = await CategoryService.getCategory(id);
-  oldCategory.subcategories.forEach(async subcat => {
+  oldCategory.subcategories.forEach(async (subcat) => {
     // this means that the user deleted a subcategory
-    if (!newCategory.subcategories.find(cat => cat._id == subcat._id)) {
-      await ProductsService.deleteProductsByCategoryId(subcat._id)
+    if (!newCategory.subcategories.find((cat) => cat._id == subcat._id)) {
+      await ProductsService.deleteProductsByCategoryId(subcat._id);
     }
-  })
-
+  });
+  //test
   try {
     const category = await CategoryService.updateCategory(id, newCategory);
     if (!category) {
       return res
         .status(404)
-        .json({ success: false, message: 'Category not found' });
+        .json({ success: false, message: "Category not found" });
     }
 
     res.status(200).json({ success: true, data: category });
   } catch (error) {
-    console.error('Error fetching category:', error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    console.error("Error fetching category:", error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 }
 
@@ -81,12 +81,12 @@ export async function getCategoryProducts(req, res) {
     if (!products) {
       return res
         .status(404)
-        .json({ success: false, message: 'No products found' });
+        .json({ success: false, message: "No products found" });
     }
     res.status(200).json({ success: true, data: products });
   } catch (error) {
-    console.error('Error fetching products by category:', error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    console.error("Error fetching products by category:", error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 }
 
@@ -98,7 +98,7 @@ export async function deleteCategory(req, res) {
     if (!deletedCategory) {
       return res
         .status(404)
-        .json({ success: false, message: 'Category not found' });
+        .json({ success: false, message: "Category not found" });
     }
 
     // delete all products related to a specific Category
@@ -106,8 +106,7 @@ export async function deleteCategory(req, res) {
 
     res.status(200).json({ success: true, data: deletedCategory });
   } catch (error) {
-    console.error('Error deleting category:', error);
-    res.status(500).json({ success: false, message: 'Server error' });
+    console.error("Error deleting category:", error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 }
-
